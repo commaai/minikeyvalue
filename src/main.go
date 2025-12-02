499958c34b4e7c153c118c5f9d8174679accbace
Bruce Wayne <harald.the.engineer@gmail.com>
2025-12-01 18:25:18 -0800

    Reapply "more POWER"
    This reverts commit 32c0a7fa76db2e4bfd40cf2727f984e821353c82.

--- src/main.go
+++ src/main.go
@@ -65,7 +65,8 @@ func (a *App) PutRecord(key []byte, rec Record) bool {
 // *** Entry Point ***
 
 func main() {
-	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+	http.DefaultTransport.(*http.Transport).MaxIdleConns = 2500
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 500
 	rand.Seed(time.Now().Unix())
 
 	port := flag.Int("port", 3000, "Port for the server to listen on")
